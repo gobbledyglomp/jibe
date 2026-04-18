e13194a0c042ca127e8264cee883741ece9038c7
Gobbledyglomp <skoobidyboo@pm.me>
2026-04-18 10:05:20 +0200

    feat(daemon): add config module with network and mDNS constants

---
+++ daemon/jibe/config.py
@@ -0,0 +1,9 @@
+""" Daemon-wide constants and configuration. """
+
+DEFAULT_PORT = 8765
+
+SERVICE_TYPE = "_jibe._tcp.local."
+SERVICE_NAME = "Jibe"
+
+LOG_FORMAT = "%(asctime)s [%(levelname)s] %(name)s: %(message)s"
+LOG_DATE_FORMAT = "%H:%M:%S"
